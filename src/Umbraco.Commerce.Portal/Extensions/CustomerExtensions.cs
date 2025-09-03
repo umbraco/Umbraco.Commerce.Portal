d0c8f974bdd3fa1c54f8e15d5e6a05811c830c84
Adrian Cojocariu <aco@umbraco.dk>
2025-09-03 17:00:53 +0300

    Use OrderPropertyConfig to determine order property aliases.

--- src/Umbraco.Commerce.Portal/Extensions/CustomerExtensions.cs
+++ src/Umbraco.Commerce.Portal/Extensions/CustomerExtensions.cs
@@ -6,40 +6,40 @@ namespace Umbraco.Commerce.Portal.Extensions;
 
 public static class CustomerExtensions
 {
-    public static CustomerModel GetCustomerModel(this OrderReadOnly order) =>
+    public static CustomerModel GetCustomerModel(this OrderReadOnly order, OrderPropertyConfig orderPropertyConfig) =>
         new CustomerModel
         {
             FirstName = order.CustomerInfo.FirstName,
             LastName = order.CustomerInfo.LastName,
             Email = order.CustomerInfo.Email,
-            Telephone = order.Properties["billingTelephone"],
+            Telephone = order.Properties[orderPropertyConfig.Customer.Telephone.Alias],
         };
 
-    public static AddressModel GetBillingAddressModel(this OrderReadOnly order)
+    public static AddressModel GetBillingAddressModel(this OrderReadOnly order, OrderPropertyConfig orderPropertyConfig)
     {
         // This extension method should not be used in a context where order is null.
         ArgumentNullException.ThrowIfNull(order);
 
         return new AddressModel
         {
-            AddressLine1 = order.Properties["billingAddressLine1"],
-            AddressLine2 = order.Properties["billingAddressLine2"],
-            City = order.Properties["billingCity"],
-            ZipCode = order.Properties["billingZipCode"],
+            AddressLine1 = order.Properties[orderPropertyConfig.Billing.AddressLine1.Alias],
+            AddressLine2 = order.Properties[orderPropertyConfig.Billing.AddressLine2.Alias],
+            City = order.Properties[orderPropertyConfig.Billing.City.Alias],
+            ZipCode = order.Properties[orderPropertyConfig.Billing.ZipCode.Alias],
         };
     }
 
-    public static AddressModel GetShippingAddressModel(this OrderReadOnly order)
+    public static AddressModel GetShippingAddressModel(this OrderReadOnly order, OrderPropertyConfig orderPropertyConfig)
     {
         // This extension method should not be used in a context where order is null.
         ArgumentNullException.ThrowIfNull(order);
 
         return new AddressModel
         {
-            AddressLine1 = order.Properties["shippingAddressLine1"],
-            AddressLine2 = order.Properties["shippingAddressLine2"],
-            City = order.Properties["shippingCity"],
-            ZipCode = order.Properties["shippingZipCode"],
+            AddressLine1 = order.Properties[orderPropertyConfig.Shipping.AddressLine1.Alias],
+            AddressLine2 = order.Properties[orderPropertyConfig.Shipping.AddressLine2.Alias],
+            City = order.Properties[orderPropertyConfig.Shipping.City.Alias],
+            ZipCode = order.Properties[orderPropertyConfig.Shipping.ZipCode.Alias],
         };
     }
 
